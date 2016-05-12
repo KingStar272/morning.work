ac75dd773738dcbc5e852fe2c96d60a4a9e88e80
Zongmin Lei <leizongmin@gmail.com>
2016-05-12 12:17:03 +0800

    test

--- demo/redis_client/test.js
+++ demo/redis_client/test.js
@@ -4,6 +4,8 @@ const Redis = require('./');
 
 const client = new Redis();
 
+client.get('a', console.log);
+
 client.setex('a 123 456')
   .then(ret => console.log('success', ret))
   .catch(err => console.log('error', err))
