7b1ef692cfadb880004f66ec847ec85559d4fa94
Zongmin Lei <leizongmin@gmail.com>
2016-05-12 23:57:21 +0800

    更新模板

--- build/authors.js
+++ build/authors.js
@@ -2,12 +2,19 @@ export default {
   '老雷': {
     default: {
       name: '老雷',
-      about: 'Node.js/JavaScript程序员',
+      about: `
+        Node.js/JavaScript程序员，
+        <a href="http://nodejs.ucdok.com/">《Node.js实战》</a>作者之一<br>
+        GitHub: <a href="https://github.com/leizongmin">https://github.com/leizongmin</a>
+      `,
       link: 'http://ucdok.com',
     },
     en: {
       name: 'Zongmin Lei',
-      about: 'Node.js/JavaScript Programmer',
+      about: `
+        Node.js/JavaScript Programmer<br>
+        GitHub: <a href="https://github.com/leizongmin">https://github.com/leizongmin</a>
+      `,
       link: 'http://ucdok.com',
     }
   }
