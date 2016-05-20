cedf2c6a0dfc4ce2d801a5a481851f53204ed0cf
Zongmin Lei <leizongmin@gmail.com>
2016-05-20 16:13:30 +0800

    update template

--- build/authors.js
+++ build/authors.js
@@ -3,10 +3,10 @@ export default {
     default: {
       name: '老雷',
       about: `
-        Node.js/JavaScript/Go开发者，
-        <a href="http://www.superid.me/">一登</a>后端架构师，
+        Node.js/JavaScript/Go开发者、
+        <a href="http://www.superid.me/">一登</a>后端架构师、
         <a href="http://nodejs.ucdok.com/">《Node.js实战》</a>作者之一<br>
-        个人主页: <a href="http://ucdok.com">http://ucdok.com<br>
+        个人主页: <a href="http://ucdok.com">http://ucdok.com</a><br>
         GitHub: <a href="https://github.com/leizongmin">https://github.com/leizongmin</a><br>
       `,
     },
